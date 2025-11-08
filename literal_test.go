a19ae8817dec6845559e1cf787674267c5d8005d
Alysson Ribeiro <15274059+sonalys@users.noreply.github.com>
2025-11-08 09:48:11 +0100

    add literal definition resolving tests

---
+++ literal_test.go
@@ -0,0 +1,186 @@
+package gon_test
+
+import (
+	"testing"
+
+	"github.com/sonalys/gon"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Literal_Definition(t *testing.T) {
+	t.Run("structs/should resolve pointer structs", func(t *testing.T) {
+		type str struct {
+			Value int `gon:"value"`
+		}
+
+		scope, err := gon.
+			NewScope().
+			WithDefinitions(gon.Definitions{
+				"var": gon.Literal(&str{
+					Value: 5,
+				}),
+			})
+		require.NoError(t, err)
+
+		gotValue, ok := scope.Definition("var.value")
+		require.True(t, ok)
+
+		require.Equal(t, 5, gotValue.Value())
+	})
+
+	t.Run("structs/should resolve pointer fields", func(t *testing.T) {
+		type str struct {
+			Value *int `gon:"value"`
+		}
+
+		expected := 5
+
+		scope, err := gon.
+			NewScope().
+			WithDefinitions(gon.Definitions{
+				"var": gon.Literal(str{
+					Value: &expected,
+				}),
+			})
+		require.NoError(t, err)
+
+		gotValue, ok := scope.Definition("var.value")
+		require.True(t, ok)
+
+		require.Equal(t, &expected, gotValue.Value())
+	})
+
+	t.Run("structs/should resolve nested fields", func(t *testing.T) {
+		type attribute struct {
+			Value int `gon:"value"`
+		}
+
+		type str struct {
+			Attribute attribute `gon:"attribute"`
+		}
+
+		expected := 5
+
+		scope, err := gon.
+			NewScope().
+			WithDefinitions(gon.Definitions{
+				"var": gon.Literal(str{
+					Attribute: attribute{
+						Value: expected,
+					},
+				}),
+			})
+		require.NoError(t, err)
+
+		gotValue, ok := scope.Definition("var.attribute.value")
+		require.True(t, ok)
+
+		require.Equal(t, expected, gotValue.Value())
+	})
+
+	t.Run("structs/should resolve nested pointer fields", func(t *testing.T) {
+		type attribute struct {
+			Value int `gon:"value"`
+		}
+
+		type str struct {
+			Attribute *attribute `gon:"attribute"`
+		}
+
+		expected := 5
+
+		scope, err := gon.
+			NewScope().
+			WithDefinitions(gon.Definitions{
+				"var": gon.Literal(str{
+					Attribute: &attribute{
+						Value: expected,
+					},
+				}),
+			})
+		require.NoError(t, err)
+
+		gotValue, ok := scope.Definition("var.attribute.value")
+		require.True(t, ok)
+
+		require.Equal(t, expected, gotValue.Value())
+	})
+
+	t.Run("maps/should resolve pointer structs", func(t *testing.T) {
+		type str map[string]int
+
+		scope, err := gon.
+			NewScope().
+			WithDefinitions(gon.Definitions{
+				"var": gon.Literal(&str{
+					"value": 5,
+				}),
+			})
+		require.NoError(t, err)
+
+		gotValue, ok := scope.Definition("var.value")
+		require.True(t, ok)
+
+		require.Equal(t, 5, gotValue.Value())
+	})
+
+	t.Run("maps/should resolve pointer fields", func(t *testing.T) {
+		type str map[string]*int
+
+		expected := 5
+
+		scope, err := gon.
+			NewScope().
+			WithDefinitions(gon.Definitions{
+				"var": gon.Literal(str{
+					"value": &expected,
+				}),
+			})
+		require.NoError(t, err)
+
+		gotValue, ok := scope.Definition("var.value")
+		require.True(t, ok)
+
+		require.Equal(t, &expected, gotValue.Value())
+	})
+
+	t.Run("maps/should resolve nested fields", func(t *testing.T) {
+		expected := 5
+
+		scope, err := gon.
+			NewScope().
+			WithDefinitions(gon.Definitions{
+				"var": gon.Literal(map[string]map[string]int{
+					"attribute": {
+						"value": expected,
+					},
+				}),
+			})
+		require.NoError(t, err)
+
+		gotValue, ok := scope.Definition("var.attribute.value")
+		require.True(t, ok)
+
+		require.Equal(t, expected, gotValue.Value())
+	})
+
+	t.Run("maps/should resolve nested pointer fields", func(t *testing.T) {
+		expected := 5
+
+		scope, err := gon.
+			NewScope().
+			WithDefinitions(gon.Definitions{
+				"var": gon.Literal(map[string]*map[string]int{
+					"attribute": {
+						"value": expected,
+					},
+				}),
+			})
+		require.NoError(t, err)
+
+		gotValue, ok := scope.Definition("var.attribute.value")
+		require.True(t, ok)
+
+		require.Equal(t, expected, gotValue.Value())
+	})
+}
